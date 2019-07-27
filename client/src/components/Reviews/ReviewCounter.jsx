0f24ac43a7c5f33ca96a155633e0a1c5d7724350
Cali Tabilas <ctabilas23@gmail.com>
2019-07-27 17:20:46 -0400

    refactoring to hooks

--- client/src/components/Reviews/ReviewCounter.jsx
+++ client/src/components/Reviews/ReviewCounter.jsx
@@ -1,12 +1,9 @@
 import { connect } from 'react-redux';
 import React from 'react';
 
-const ReviewCounter = ({ reviews, meta }) => {
-  console.log(`meta from counter`, meta);
-
+const ReviewCounter = ({ meta }) => {
   const countReviews = (ratings) => {
     const vals = Object.values(ratings);
-    console.log(`vals`, vals);
     return vals.reduce((a, b) => {
       return a + b;
     });
--- client/src/components/Reviews/ReviewsWrapper.jsx
+++ client/src/components/Reviews/ReviewsWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -6,35 +6,70 @@ import RevParentComponent from './RevParentComponent.jsx';
 
 import { fetchReviews, fetchMeta } from '../../actions/reviewsActions';
 
-class ReviewsWrapper extends Component {
-  constructor(props) {
-    super(props);
+// class ReviewsWrapper extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      //
-    };
-  }
+//     this.state = {
+//       //
+//     };
+//   }
 
-  componentDidMount() {
-    const {
-      fetchReviews,
-      fetchMeta,
-      location: { pathname }
-      // reviews
-    } = this.props;
-    // FIXME: get id from route
+//   componentDidMount() {
+//     const {
+//       fetchReviews,
+//       fetchMeta,
+//       location: { pathname }
+//       // reviews
+//     } = this.props;
+//     // FIXME: get id from route
+//     fetchReviews(pathname);
+//     fetchMeta(pathname);
+//   }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <RevParentComponent />
+//         {/* {console.log(`results inside wrapper`, reviews.data.results)} */}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const ReviewsWrapper = (props) => {
+  // console.log(`props`, props);
+  const {
+    fetchReviews,
+    fetchMeta,
+    location: { pathname }
+    // reviews
+  } = props;
+
+  // const [] = useState();
+
+  useEffect(() => {
     fetchReviews(pathname);
     fetchMeta(pathname);
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <RevParentComponent />
-        {/* {console.log(`results inside wrapper`, reviews.data.results)} */}
-      </React.Fragment>
-    );
-  }
-}
+  });
+
+  // componentDidMount() {
+  //   const {
+  //     fetchReviews,
+  //     fetchMeta,
+  //     location: { pathname }
+
+  //   } = this.props;
+
+  //   fetchReviews(pathname);
+  //   fetchMeta(pathname);
+  // }
+
+  return (
+    <React.Fragment>
+      <RevParentComponent />
+    </React.Fragment>
+  );
+};
 
 ReviewsWrapper.propTypes = {
   fetchReviews: PropTypes.func.isRequired,
